d355dda38eda446334ee0f5c1fd62259ec963655
TishaMehta <105579992+TishaMehta@users.noreply.github.com>
2023-08-07 18:30:52 +0530

    Create Program.cs

---
+++ Lab4/Program.cs
@@ -0,0 +1,47 @@
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Class1 c1 = new Class1();
+           // c1.getZeroDIvisionError();
+
+           // Class2 c2 = new Class2();
+            //c2.EndexOutOfBoundErro();
+
+           // Class3 c3 = new Class3();
+            //c3.throwError();
+
+            //Calculate c4 = new Calculate();
+            //c4.SumOfTwo(12, 2);
+            //c4.SumOfThree(1, 2, 4);
+
+           // Class5 c5 = new Class5();
+            //c5.getdetails();
+            //c5.addition();
+            //c5.sub();
+
+           // Class6 c6 = new Class6();
+           // c6.getDetails();
+           // c6.Circle();
+           // c6.Square();
+           // c6.Triangle();
+
+           // Class7 c7 = new Class7();
+            //c7.stringMethod("Tisha");
+
+            Class8 c8 = new Class8();
+            c8.toLower("TISHA MEhtaa");
+            c8.toUpper("tisha");
+
+            //Class9 c9 = new Class9();
+            //c9.getString();
+            //c9.findMaxWord();
+
+            Class10 c10 = new Class10();
+            c10.invert("TisHa MehTa");
+
+        }
+    }
+}
